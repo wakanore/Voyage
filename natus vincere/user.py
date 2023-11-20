b181415214d63363abb22d1d91548d512d2ef1a9
wakanore <138709595+wakanore@users.noreply.github.com>
2023-11-20 10:38:44 +0300

    Update user.py

--- natus vincere/user.py
+++ natus vincere/user.py
@@ -4,20 +4,41 @@ from sqlalchemy.orm import Session
 from starlette.status import HTTP_400_BAD_REQUEST
 
 from model import User
-from schemas import UserCreate
 
+import random
+def uniqueid():
+    seed = random.getrandbits(32)
+    while True:
+       yield seed
+       seed += 1
 
-def register(db: Session, user_data: UserCreate):
+def register(db: Session, user_data: User):
+    unique_sequence = uniqueid()
     if db.scalar(select(User).where(User.email == user_data.email)):
         raise HTTPException(
             status_code=HTTP_400_BAD_REQUEST,
             detail="User with this email already exists!"
         )
-    user = User(email=user_data.email)
-    user.hashed_password = user_data.password
+    user = User(
+        id=user_data.unique_sequence,
+        username=user_data.username,
+        email=user_data.email,
+        Image=user_data.Image,
+        type=user_data.type,
+        age=user_data.age,
+        registrationTime=user_data.registrationTime,
+    )
+    user.password = user_data.password
     db.add(user)
     db.commit()
     return {
         "id": user.id,
         "email": user.email,
+        "id": user.id,
+        "username": user.username,
+        "emale": user.emale,
+        "Image": user.Image,
+        "type" : user.type,
+        "age": user.age,
+        "registrationTime": user.registrationTime
     }
