d56c85abf778d05990867c8e1b0afa8f2a522033
wakanore <138709595+wakanore@users.noreply.github.com>
2023-11-21 22:19:42 +0300

    Update main.py

--- natus vincere/main.py
+++ natus vincere/main.py
@@ -5,8 +5,12 @@ from pydantic import BaseModel
 from pydantic.dataclasses import dataclass
 from fastapi import FastAPI, APIRouter, Path
 from user_router import user_router
+from hotel import hotel_router
 
 app = FastAPI()
 
 
 app.include_router(user_router)
+
+app.include_router(hotel_router)
+
